8ef85f9f998a0475cc46cd727f05d8b12a9940b4
Kaloyan Marinov <kaloyan.marinov@protonmail.com>
2023-12-31 10:36:55 +0100

    6: afford `utility-scripts/populate-db.sh` access to the environment variables that it needs and that are specified in `.env`

--- README.md
+++ README.md
@@ -231,6 +231,7 @@ Python 3.8.3 (v3.8.3:6f8c8320e9, May 13 2020, 16:29:34)
 [Clang 6.0 (clang-600.0.57)] on darwin
 Type "help", "copyright", "credits" or "license" for more information.
 (InteractiveConsole)
+>>> import os
 >>> from django.contrib.auth.models import User
 >>> user = User.objects.create_user(
     os.environ.get("USERNAME"),
--- utility-scripts/populate-db.sh
+++ utility-scripts/populate-db.sh
@@ -1,3 +1,5 @@
+source .env
+
 # Arrange for every command to be printed before it is executed.
 set -o xtrace
 
@@ -16,6 +18,11 @@ curl \
    }" \
    localhost:8000/api/sign_in
 
+# # ...
+# < HTTP/1.1 204 No Content
+# # ...
+# <no output>
+# ```
 
 # Extract the CSRF token,
 # which is contained in the preceding command's output.
@@ -29,7 +36,6 @@ else
    echo "$line_with_csrf_token doesn't match" >&2
 fi
 
-
 # Extract the Session ID,
 # which is contained in the preceding command's output.
 regex="Set-Cookie:  sessionid=([0-9a-zA-Z]+);"
@@ -42,195 +48,192 @@ else
    echo "$line_with_session_id doesn't match" >&2
 fi
 
-# rm ${TEMP_FILE}
-# 
-# http \
-#    localhost:8000/api/tasks \
-#    cookie:sessionid=${SESSION_ID}
+rm ${TEMP_FILE}
 
-# echo ""
-# curl \
-#    --verbose \
-#    --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
-#    --header "X-CSRFToken: ${CSRF_TOKEN}" \
-#    localhost:8000/api/tasks \
-#    | json_pp
+echo ""
+curl \
+   --verbose \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   localhost:8000/api/tasks \
+   | json_pp
 
-# # # ...
-# # < HTTP/1.1 200 OK
-# # # ...
-# # {
-# #    "items" : []
-# # }
+# # ...
+# < HTTP/1.1 200 OK
+# # ...
+# {
+#    "items" : []
+# }
 
-# echo ""
-# curl \
-#    --verbose \
-#    --request POST \
-#    --header "Content-Type: application/json" \
-#    --data '{
-#       "category": "health"
-#    }' \
-#    localhost:8000/api/tasks \
-#    | json_pp
+echo ""
+curl \
+   --verbose \
+   --request POST \
+   --header "Content-Type: application/json" \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   --data '{
+      "category": "health"
+   }' \
+   localhost:8000/api/tasks \
+   | json_pp
 
-# # # ...
-# # < HTTP/1.1 400 Bad Request
-# # # ...
-# # {
-# #    "error" : "Bad Request",
-# #    "message" : "The request body has to provide values for each of 'category' and 'description'."
-# # }
+# # ...
+# < HTTP/1.1 400 Bad Request
+# # ...
+# {
+#    "error" : "Bad Request",
+#    "message" : "The request body has to provide values for each of 'category' and 'description'."
+# }
 
-# echo ""
-# curl \
-#    --verbose \
-#    --request POST \
-#    --header "Content-Type: application/json" \
-#    --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
-#    --header "X-CSRFToken: ${CSRF_TOKEN}" \
-#    --data '{
-#       "category": "health",
-#       "description": "go to the doctor"
-#    }' \
-#    localhost:8000/api/tasks \
-#    | json_pp
+echo ""
+curl \
+   --verbose \
+   --request POST \
+   --header "Content-Type: application/json" \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   --data '{
+      "category": "health",
+      "description": "go to the doctor"
+   }' \
+   localhost:8000/api/tasks \
+   | json_pp
 
-# # # ...
-# # < HTTP/1.1 201 Created
-# # # ...
-# # {
-# #    "category" : "health",
-# #    "description" : "go to the doctor",
-# #    "id" : 1
-# # }
+# # ...
+# < HTTP/1.1 201 Created
+# # ...
+# {
+#    "category" : "health",
+#    "description" : "go to the doctor",
+#    "id" : 1
+# }
 
 
 
-# echo ""
-# curl \
-#    --verbose \
-#    --request POST \
-#    --header "Content-Type: application/json" \
-#    --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
-#    --header "X-CSRFToken: ${CSRF_TOKEN}" \
-#    --data '{
-#       "category": "work",
-#       "description": "build a web application using Django"
-#    }' \
-#    localhost:8000/api/tasks \
-#    | json_pp
+echo ""
+curl \
+   --verbose \
+   --request POST \
+   --header "Content-Type: application/json" \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   --data '{
+      "category": "work",
+      "description": "build a web application using Django"
+   }' \
+   localhost:8000/api/tasks \
+   | json_pp
 
-# # # ...
-# # < HTTP/1.1 201 Created
-# # # ...
-# # {
-# #    "category" : "work",
-# #    "description" : "build a web application using Django",
-# #    "id" : 2
-# # }
+# # ...
+# < HTTP/1.1 201 Created
+# # ...
+# {
+#    "category" : "work",
+#    "description" : "build a web application using Django",
+#    "id" : 2
+# }
 
-# echo ""
-# curl \
-#    --verbose \
-#    --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
-#    --header "X-CSRFToken: ${CSRF_TOKEN}" \
-#    localhost:8000/api/tasks \
-#    | json_pp
+echo ""
+curl localhost:8000/api/tasks \
+   --verbose \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   | json_pp
 
-# # # ...
-# # < HTTP/1.1 200 OK
-# # # ...
-# # {
-# #    "items" : [
-# #       {
-# #          "category" : "health",
-# #          "description" : "go to the doctor",
-# #          "id" : 1
-# #       },
-# #       {
-# #          "category" : "work",
-# #          "description" : "build a web application using Django",
-# #          "id" : 2
-# #       }
-# #    ]
-# # }
-
+# # ...
+# < HTTP/1.1 200 OK
+# # ...
+# {
+#    "items" : [
+#       {
+#          "category" : "health",
+#          "description" : "go to the doctor",
+#          "id" : 1
+#       },
+#       {
+#          "category" : "work",
+#          "description" : "build a web application using Django",
+#          "id" : 2
+#       }
+#    ]
+# }
 
 
-# echo ""
-# curl \
-#    --verbose \
-#    --request POST \
-#    --header "Content-Type: application/json" \
-#    --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
-#    --header "X-CSRFToken: ${CSRF_TOKEN}" \
-#    --data '{
-#       "category": "VACATON",
-#       "description": "look up INTRESTING towns in SICLY to VISITT"
-#    }' \
-#    localhost:8000/api/tasks \
-#    | json_pp
 
-# # # ...
-# # < HTTP/1.1 201 Created
-# # # ...
-# # {
-# #    "category" : "VACATON",
-# #    "description" : "look up INTRESTING towns in SICLY to VISITT",
-# #    "id" : 3
-# # }
+echo ""
+curl \
+   --verbose \
+   --request POST \
+   --header "Content-Type: application/json" \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   --data '{
+      "category": "VACATON",
+      "description": "look up INTRESTING towns in SICLY to VISITT"
+   }' \
+   localhost:8000/api/tasks \
+   | json_pp
 
-# echo ""
-# curl \
-#    --verbose \
-#    --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
-#    --header "X-CSRFToken: ${CSRF_TOKEN}" \
-#    localhost:8000/api/tasks/3 \
-#    | json_pp
+# # ...
+# < HTTP/1.1 201 Created
+# # ...
+# {
+#    "category" : "VACATON",
+#    "description" : "look up INTRESTING towns in SICLY to VISITT",
+#    "id" : 3
+# }
 
-# # # ...
-# # < HTTP/1.1 200 OK
-# # # ...
-# # {
-# #    "category" : "VACATON",
-# #    "description" : "look up INTRESTING towns in SICLY to VISITT",
-# #    "id" : 3
-# # }
+echo ""
+curl \
+   --verbose \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   localhost:8000/api/tasks/3 \
+   | json_pp
 
-# echo ""
-# curl \
-#    --verbose \
-#    --request PUT \
-#    --header "Content-Type: application/json" \
-#    --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
-#    --header "X-CSRFToken: ${CSRF_TOKEN}" \
-#    --data '{
-#       "category": "vacation",
-#       "description": "look up interesting towns in Sicily to visit"
-#    }' \
-#    localhost:8000/api/tasks/3 \
-#    | json_pp
+# # ...
+# < HTTP/1.1 200 OK
+# # ...
+# {
+#    "category" : "VACATON",
+#    "description" : "look up INTRESTING towns in SICLY to VISITT",
+#    "id" : 3
+# }
 
-# # # ...
-# # < HTTP/1.1 200 OK
-# # # ...
-# # {
-# #    "category" : "vacation",
-# #    "description" : "look up interesting towns in Sicily to visit",
-# #    "id" : 3
-# # }
+echo ""
+curl \
+   --verbose \
+   --request PUT \
+   --header "Content-Type: application/json" \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   --data '{
+      "category": "vacation",
+      "description": "look up interesting towns in Sicily to visit"
+   }' \
+   localhost:8000/api/tasks/3 \
+   | json_pp
 
-# echo ""
-# curl \
-#    --verbose \
-#    --request DELETE \
-#    --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
-#    --header "X-CSRFToken: ${CSRF_TOKEN}" \
-#    localhost:8000/api/tasks/2
+# # ...
+# < HTTP/1.1 200 OK
+# # ...
+# {
+#    "category" : "vacation",
+#    "description" : "look up interesting towns in Sicily to visit",
+#    "id" : 3
+# }
 
-# # # ...
-# # < HTTP/1.1 204 No Content
-# # # ...
-# # <no output>
-# # ```
+echo ""
+curl \
+   --verbose \
+   --request DELETE \
+   --header "Cookie: sessionid=${SESSION_ID}; csrftoken=${CSRF_TOKEN}" \
+   --header "X-CSRFToken: ${CSRF_TOKEN}" \
+   localhost:8000/api/tasks/2
+
+# # ...
+# < HTTP/1.1 204 No Content
+# # ...
+# <no output>
+# ```
